8bb026b516be985b7e0c4b0f2234b0ad5756013d
Eddy Page <eddypage@gmail.com>
2019-07-09 16:20:14 -0400

    Commented digit_id_cnn.py for clarity

--- digit_id_cnn.py
+++ digit_id_cnn.py
@@ -12,15 +12,17 @@ from keras.models import Model
 import keras.backend as K
 K.set_image_data_format('channels_last')
 
-
+# Import digits dataset from sklearn.  Each example is an 8x8 grayscale image of a single digit number
 X, y = load_digits(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Reshape and normalize inputs
 X_train = np.reshape(X_train, (1437, 8, 8, 1))
 X_train = X_train/16
 X_test = np.reshape(X_test, (360, 8, 8, 1))
 X_test = X_test/16
 
-
+# Convert classification numbers to one-hot representations
 y_oh_train = to_categorical(y_train)
 y_oh_test = to_categorical(y_test)
 
@@ -29,35 +31,43 @@ def DigitID(input_shape):
     # Define the input placeholder as a tensor with shape input_shape. Think of this as your input image!
     X_input = Input(input_shape)
 
-    # Zero-Padding: pads the border of X_input with zeroes
+    # Pads the border of X_input with zeroes to ensure Conv2D layer's output is still 8x8
     X = ZeroPadding2D((1, 1))(X_input)
 
-    # CONV -> BN -> RELU Block applied to X
+    # A single Conv2D/BatchNorm/Relu block applied to X_input -> X
     X = Conv2D(16, (3, 3), strides=(1, 1), name = 'conv0')(X_input)
     X = BatchNormalization(axis=3, name='bn0')(X)
     X = Activation('relu')(X)
 
-    # MAXPOOL
+    # Max pooling to compress activation data
     X = MaxPooling2D((2, 2), name='max_pool')(X)
 
-    # FLATTEN X (means convert it to a vector) + FULLYCONNECTED
+    # Unroll X activations and passing through a single, fully-connected neural layer
     X = Flatten()(X)
     X = Dense(activation='sigmoid', name='fc', units=10)(X)
 
-    # Create model. This creates your Keras model instance, you'll use this instance to train/test the model.
+    # Create Keras model instance to train/test the model.
     model = Model(inputs=X_input, outputs=X, name='DigitID')
 
     return model
 
 
-print(y_oh_train.shape)
-
+# Create, compile and fit the model to the training data
 digit_model = DigitID((8, 8, 1))
 digit_model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 digit_model.fit(X_train, y_oh_train, epochs=40, batch_size=50)
 
-prediction = digit_model.evaluate(X_test, y_oh_test, batch_size=32, verbose=1, sample_weight=None)
+# Check how well the model generalizes using test set.  To optimize hyperparameters, I would use a CV set
+train_accuracy = digit_model.evaluate(X_train, y_oh_train, batch_size=32, verbose=1, sample_weight=None)
+test_accuracy = digit_model.evaluate(X_test, y_oh_test, batch_size=32, verbose=1, sample_weight=None)
 
+# Print out the results
 print()
-print ("Loss = " + str(prediction[0]))
-print ("Test Accuracy = " + str(prediction[1]))
+print ("Training Accuracy = " + str(train_accuracy[1]))
+print ("Test Accuracy = " + str(test_accuracy[1]))
+
+"""
+Last result:
+Training Accuracy = 0.9979123173277662
+Test Accuracy = 0.9861111111111112
+"""
